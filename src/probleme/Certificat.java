2009b1deab285c15f177b54691e89ddbddad441f
Monbailly Jean-serge <monbailly@a11p3.fil.univ-lille1.fr>
2014-11-17 15:51:38 +0100

    Création des packages.

---
+++ src/probleme/Certificat.java
@@ -0,0 +1,37 @@
+package probleme;
+public interface Certificat {
+
+	/**
+	 * Retourne True SSi le certificat est valide pour le problème. Doit être de
+	 * complexité polynomiale par rapport à la taille du certificat et du
+	 * problème
+	 * 
+	 * @return true ssi le certicat est valide pour le problème.
+	 */
+	public boolean correct(); // algo de vérification A de la définition NP du
+								// cours!
+
+	public boolean estDernier();
+
+	/**
+	 * Modifie aléatoirement la valeur du certificat. Chaque valeur doit pouvoir
+	 * être générée par au moins une exécution.
+	 */
+	public void alea();
+
+	/**
+	 * affiche un certificat
+	 */
+	public void affiche();
+
+	/* FACULTATIFS */
+	/**
+	 * Réinitialise le certificat au plus petit pour l’ordre.
+	 */
+	public void reset();
+
+	/**
+	 * Permet la saisie d’un certificat
+	 */
+	public void saisie();
+}
---
+++ src/probleme/PblDec.java
@@ -0,0 +1,10 @@
+package probleme;
+/**
+ * la classe abstraite des problèmes de décision.
+ */
+public abstract class PblDec {
+
+	public PblDec() {
+	}
+
+}
